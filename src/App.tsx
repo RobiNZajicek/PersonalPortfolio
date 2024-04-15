ef46f8f1b8d62f8c1a80ac2113a445bb94177492
Robin Zajicek <zajicekrobin3@gmail.com>
2024-04-15 14:39:22 +0200

    four

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,33 @@
-import React from 'react'
-import { HeaderMegaMenu } from './components/HeaderMegaMenu/HeaderMegaMenu'
-import Home from './components/Home/Home'
-import About from './components/About/About.jsx'
-import Skills from './components/Skills/Skills.jsx'
-import Projects from './components/Projects/Works.jsx'
-import WorkExperience from './components/WorkExperience/WorkExperience.jsx'
-import Experience from './components/Experience/Experience.jsx'
-import Line from './components/Line/Line.jsx'
-import { CardGradient } from './components/CardGradient/CardGradient.js'
-import CardGradientPage from './components/CardGradientPage/CardGradientPage.jsx'
-import Feedbacks from './components/Feedbacks.jsx'
-import { FooterCentered } from './components/FooterCentered/FooterCentered.js'
-import Contact from './components/Contact.jsx'
+
+import { HeaderMegaMenu } from './components/HeaderMegaMenu/HeaderMegaMenu';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Works';
+import Experience from './components/Experience/Experience';
+import Contact from './components/Contact';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n.js'; // Import your i18n configuration file
+import i18n from './i18n'; // Import your i18n configuration file
+
 const App = () => {
   return (
     <section>
       <I18nextProvider i18n={i18n}>
-      <HeaderMegaMenu/>
-      <Home/>
-       <About/>
-   
-       <Experience/>
-      <div className='container'>
-      <Skills/>
-      
-      
-      </div>
-      <Projects/>
-      <div className='relative z-0 bg-primary div-fid'> 
-
-      </div>
-     
-
-      <div className='relative z-0 bg-primary div-fid'> 
-      <Contact/>
-      </div>
-
+        <HeaderMegaMenu />
+        <Home />
+        <About />
+        <Experience />
+        <div className='container'>
+          <Skills />
+        </div>
+        <Projects />
+        <div className='relative z-0 bg-primary div-fid'></div>
+        <div className='relative z-0 bg-primary div-fid'>
+          <Contact />
+        </div>
       </I18nextProvider>
     </section>
-  )
-}
+  );
+};
 
-export default App
+export default App;
